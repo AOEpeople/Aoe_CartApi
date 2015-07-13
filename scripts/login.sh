feaa605d58b99a4130c8706463c8662b810b18a5
Lee Saferite <lee.saferite@aoe.com>
2015-07-13 20:30:38 +0000

    Fix login test script

--- scripts/login.sh
+++ scripts/login.sh
@@ -11,6 +11,6 @@ if [ "${URL}" == "" -o "${USERNAME}" == "" -o "${PASSWORD}" == "" ]; then
 fi
 
 DATA="{\"login\":\"${USERNAME}\", \"password\":\"${PASSWORD}\"}"
-SESSION=`curl -s -H "Cookie: ${COOKIE}" -H "Content-Type: application/json" -X POST -d "${DATA}" -D - "${URL}/session/customer" | grep 'Set-Cookie' | tail -n1 | sed -En 's/^Set-Cookie: frontend=([^;]+).*/\1/p'`
+SESSION=`curl -s -H "Cookie: ${COOKIE}" -H "Content-Type: application/json" -X POST -d "${DATA}" -D - "${URL}/session/customer" | grep 'Set-Cookie: frontend=' | tail -n1 | sed -En 's/^Set-Cookie: frontend=([^;]+).*/\1/p'`
 
 echo $SESSION
