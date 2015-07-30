1f09c1bede47fdd00af0e88ce22de968b53ce6bf
Lee Saferite <lee.saferite@aoe.com>
2015-07-30 17:56:41 +0000

    An unsaved quote doesn't have a currency code

--- app/code/community/Aoe/CartApi/Model/Resource.php
+++ app/code/community/Aoe/CartApi/Model/Resource.php
@@ -150,6 +150,9 @@ abstract class Aoe_CartApi_Model_Resource extends Mage_Api2_Model_Resource
     {
         // This makes me a bit nervous
         $currencyCode = $this->loadQuote()->getQuoteCurrencyCode();
+        if (empty($currencyCode)) {
+            $currencyCode = $this->loadQuote()->getStore()->getDefaultCurrencyCode();
+        }
 
         if (empty($typeMap)) {
             $typeMap = $this->attributeTypeMap;
