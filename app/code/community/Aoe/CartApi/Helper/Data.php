7558fa15c38b50c54dda8e451b61cbe136b94722
Clemens Queissner <clemens.queissner@cq-design.de>
2016-05-27 12:54:33 -0700

    Fixes an error where the totals are collected two times and promo codes are also apply to times

--- app/code/community/Aoe/CartApi/Helper/Data.php
+++ app/code/community/Aoe/CartApi/Helper/Data.php
@@ -67,13 +67,15 @@ class Aoe_CartApi_Helper_Data extends Mage_Core_Helper_Data
 
         $quote->getShippingAddress()->setCollectShippingRates(true);
 
-        /** Dirty workaround where the detection of the cache entry where explicit enough THX magento **/
-        $quote->getShippingAddress()->unsetData('cached_items_all');
-        $quote->getShippingAddress()->unsetData('cached_items_nominal');
-        $quote->getShippingAddress()->unsetData('cached_items_nonnominal');
+        if($quote->getItemsCount() == 0) {
+            /** Dirty workaround where the detection of the cache entry where explicit enough THX magento **/
+            $quote->getShippingAddress()->unsetData('cached_items_all');
+            $quote->getShippingAddress()->unsetData('cached_items_nominal');
+            $quote->getShippingAddress()->unsetData('cached_items_nonnominal');
 
-        /** Force recollection of totals on every run */
-        $quote->setTotalsCollectedFlag(false);
+            /** Force recollection of totals on every run */
+            $quote->setTotalsCollectedFlag(false);
+        }
 
         $quote->collectTotals();
 
